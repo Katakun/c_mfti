879367d82634b759d08c8435ec76ecff778e8eed
Alexandr Ivanov <k4t4kun@gmail.com>
2023-12-21 12:05:11 +0900

    add pointer

---
+++ 4_func_point/4_7_1_pointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 123;
+    int* p = &x;
+    int** pp = &p;
+
+    printf("x = %d\n", x);
+    printf("p = %p\n", p);
+    printf("pp = %p\n", pp);
+}
