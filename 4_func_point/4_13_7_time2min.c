9b1ad0309d9afdd922023b5fec0b2b7ba7418aa3
Alexandr Ivanov <k4t4kun@gmail.com>
2023-12-24 14:16:26 +0900

    4148 fib

--- 4_func_point/4_13_7_time2min.c
+++ 4_func_point/4_13_7_time2min.c
@@ -1,5 +1,5 @@
 #define cube(x) x* x* x
 #define time2min(h, m) (60 * (h) + (m))
-#define min2time(mm, h, m) (h = (mm) / 60) / (m = (mm) % 60)
+#define min2time(mm, h, m) (h = ((mm) / 60 % 24)) / (m = (mm) % 60)
 
 y = cube(2);  // y = 2*2*2;
