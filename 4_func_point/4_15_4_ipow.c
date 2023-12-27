448e8c02e569ec93c6a4e753d2ee31a5a0410796
Alexandr Ivanov <k4t4kun@gmail.com>
2023-12-27 14:13:37 +0900

    4 15 4 ipow work

--- 4_func_point/4_15_4_ipow.c
+++ 4_func_point/4_15_4_ipow.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
 long long int ipow(long long int a, unsigned int n, unsigned int* depth) {
+    (*depth)++;
+    printf("n = %02d, depth = %d\n", n, *depth);
     if (n == 1) return a;
     if (n == 0) return 1;
 
-    // a = a * a;
-
-    if (n % 2 == 0) {
-        n = n / 2;
-        return ipow(a, n, depth) * ipow(a, n, depth);
-    } else {
-        n = n - 1;
-        return a * ipow(a, n, depth);
-    }
-
-    (*depth)++;
+    if (n % 2 == 0)
+        return ipow(a * a, n / 2, depth);
+    else
+        return a * ipow(a, n - 1, depth);
 }
 
 int main() {
+    unsigned int d;
     int a, n;
-    unsigned int d = 7;
+    // int d = 7;
     scanf("%d%d", &a, &n);
-    printf("res = %llu, d = %d\n", ipow(a, n, &d), d);
+    printf("\nres = %llu, d = %d\n", ipow(a, n, &d), d);
     return 0;
 }
