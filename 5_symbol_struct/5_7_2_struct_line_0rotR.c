3d163ece2058ad78a56002f38b707be5de56c3d8
Alexandr Ivanov <k4t4kun@gmail.com>
2024-01-03 17:42:17 +0900

    line rotate done

--- 5_symbol_struct/5_7_2_struct_line_0rotR.c
+++ 5_symbol_struct/5_7_2_struct_line_0rotR.c
@@ -1,4 +1,6 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     int x;
@@ -21,7 +23,31 @@ int main() {
 
     scanLine(&t);
     // тут должен быть вызов функции rotRLine
+    rotRLine(&t);
     printLine(t);
 
     return 0;
+}
+
+void scanLine(Line* t) {
+    scanf("%d %d %d %d", &t->a.x, &t->a.y, &t->b.x, &t->b.y);
+    t->len = distance(t->a, t->b);
+}
+
+void printLine(Line t) { printf("%d %d %d %d %.3f\n", t.a.x, t.a.y, t.b.x, t.b.y, t.len); }
+
+float distance(Point a, Point b) {
+    int x_side = abs(a.x - b.x);
+    int y_side = abs(a.y - b.y);
+    float distance = sqrt(pow(x_side, 2) + pow(y_side, 2));
+    return distance;
+}
+
+void rotRLine(Line* t) {
+    int tmp = t->a.x;
+    t->a.x = t->a.y;
+    t->a.y = tmp * -1;
+    tmp = t->b.x;
+    t->b.x = t->b.y;
+    t->b.y = tmp * -1;
 }
