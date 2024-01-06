2f1fe331d0651608eee9c7b5f8a2018a94c2d70f
Alexandr <k4t4kun@gmail.com>
2024-01-06 10:34:59 +0900

    663 done

--- 6_if/6_6_2_if7.c
+++ 6_if/6_6_2_if7.c
@@ -5,5 +5,11 @@ int main() {
     int row1, row2;
     scanf("%c%d-%c%d", &col1, &row1, &col2, &row2);
     printf("%c%d-%c%d\n", col1, row1, col2, row2);
+    if (col1 == col2 || row1 == row2) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
     return 0;
 }
