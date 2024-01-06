158ec24c621746fd55d050535317de4c84f0439f
Alexandr Ivanov <k4t4kun@gmail.com>
2024-01-06 14:55:11 +0900

    6_6_7_done

--- 6_if/6_6_7_if.c
+++ 6_if/6_6_7_if.c
@@ -1,1 +1,13 @@
-if
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if ((n % 3 == 0 || n % 5 == 0) && (n % 15 != 0)) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
+    return 0;
+}
