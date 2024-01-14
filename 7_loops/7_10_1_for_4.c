0d74bc3924b60757b1038f8c5ec64b8c0a0ac26d
Alexandr <k4t4kun@gmail.com>
2024-01-14 11:55:11 +0900

    7_10_2 done

--- 7_loops/7_10_1_for_4.c
+++ 7_loops/7_10_1_for_4.c
@@ -4,8 +4,8 @@ int main() {
     int n;
     int res = 1;
     scanf("%d", &n);
-    for (int i = 1; i < n; i++) {
-        res = res * n;
+    for (int i = 1; i <= n; i++) {
+        res = res * i;
     }
     printf("%d\n", res);
     return 0;
