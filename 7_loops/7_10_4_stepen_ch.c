6c844517a5f8a5e47c7f8a69fc533a3c707bde85
Alexandr Ivanov <k4t4kun@gmail.com>
2024-01-15 11:47:12 +0900

    7_10_4 done

--- 7_loops/7_10_4_stepen_ch.c
+++ 7_loops/7_10_4_stepen_ch.c
@@ -1,12 +1,23 @@
+#include <math.h>
 #include <stdio.h>
 
 int main() {
-    int n, k;  // n - число, k - степень
-    scanf("%d %d", &n, &k);
+    unsigned long long number;  // n - число
+    int power;                  // k - степень
+    scanf("%llu %d", &number, &power);
 
-    int i = 1;
-    while (n > 0) {
+    int i = 2;
+    int result = 1;
+    while (number > 1 && i < 1000) {
+        if (number % (int)pow(i, power) == 0) {
+            number = number / pow(i, power);
+            printf("%d %llu\n", i, number);
+            result *= i;
+            i = 2;
+        } else {
+            i++;
         }
-
+    }
+    printf("%d\n", number == 1 ? result : -1);
     return 0;
 }
