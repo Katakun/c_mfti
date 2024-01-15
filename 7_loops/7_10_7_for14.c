51c828b8628a771396036dc3f419f7dee045c0cd
Alexandr Ivanov <k4t4kun@gmail.com>
2024-01-15 13:32:42 +0900

    7_10_7 done

--- 7_loops/7_10_7_for14.c
+++ 7_loops/7_10_7_for14.c
@@ -2,9 +2,17 @@
 
 int main() {
     char ch;
-    while ((ch = getchar()) != '\n') {
-        printf("%c", ch);
+    int stack = 0;
+    while ((ch = getchar()) != EOF) {
+        if (ch == '(')
+            stack++;
+        else if (ch == ')')
+            stack--;
+        if (stack < 0) {
+            break;
+        }
     }
+    printf("%s\n", stack == 0 ? "YES" : "NO");
 
     return 0;
 }
