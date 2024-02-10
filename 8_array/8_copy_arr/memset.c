8e110e8365832b0861b4599b1b440f3e0c86ca98
Alexandr Ivanov <k4t4kun@gmail.com>
2024-02-10 12:56:15 +0900

    memset

--- 8_array/8_copy_arr/memset.c
+++ 8_array/8_copy_arr/memset.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() { return 0; }
+int main() {
+  int a[] = {3, 7, -12, 4};
+  int b[10] = {1};
+
+  memset(b, 0, sizeof(b));  // заполняет массив b нулями
+
+  return 0;
+}
