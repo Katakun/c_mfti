fd40e5070d325ef6249a874b1990c9bacd51ad76
Alexandr <k4t4kun@gmail.com>
2024-02-10 16:23:26 +0900

    memset

--- 8_array/8_copy_arr/memset.c
+++ 8_array/8_copy_arr/memset.c
@@ -2,10 +2,20 @@
 #include <string.h>
 
 int main() {
-  int a[] = {3, 7, -12, 4};
-  int b[10] = {1};
+    int a[5] = {1, 1, 1, 1, 1};
+    int b[5];
 
-  memset(b, 0, sizeof(b));  // заполняет массив b нулями
+    for (size_t i = 0; i < sizeof(a) / sizeof(a[0]); i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
 
-  return 0;
+    // заполняет каждый байт массива b 1. (16843009)
+    memset(b, 1, sizeof(b));
+    for (size_t i = 0; i < sizeof(b) / sizeof(b[0]); i++) {
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
