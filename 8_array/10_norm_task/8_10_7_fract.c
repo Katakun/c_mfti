ad277c790d8609a6360cdc73f0ace3fe4ffd8754
Alexandr <k4t4kun@gmail.com>
2024-02-10 18:04:50 +0900

    R_decimal_fract

--- 8_array/10_norm_task/8_10_7_fract.c
+++ 8_array/10_norm_task/8_10_7_fract.c
@@ -1,8 +1,57 @@
+/*
+все решил PHIND.com
+*/
+
 #include <stdio.h>
 
+int gcd(int a, int b) {
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+void print_decimal(int numerator, int denominator) {
+    int remainder = numerator % denominator;
+    printf("0.");
+
+    // Периодическая часть
+    int period[denominator];
+    for (int i = 0; i < denominator; i++) {
+        period[i] = -1;
+    }
+
+    // Находим периодическую часть
+    int index = 0;
+    while (remainder != 0 && period[remainder] == -1) {
+        period[remainder] = index++;
+        remainder *= 10;
+        printf("%d", remainder / denominator);
+        remainder %= denominator;
+    }
+
+    // Печатаем периодическую часть
+    if (remainder != 0) {
+        printf("(");
+        for (int i = period[remainder]; i < index; i++) {
+            printf("%d", period[i]);
+        }
+        printf(")");
+    }
+}
+
 int main() {
-    int chislitel, znamenatel;
-    scanf("%d %d", &chislitel, &znamenatel);
+    int numerator, denominator;
+    scanf("%d %d", &numerator, &denominator);
+
+    // Упрощаем дробь
+    int g = gcd(numerator, denominator);
+    numerator /= g;
+    denominator /= g;
+
+    print_decimal(numerator, denominator);
 
     return 0;
 }
