265d84b10c5f01166c0c6b57ae681e1cc342f2e8
Alexandr Ivanov <k4t4kun@gmail.com>
2024-02-11 17:47:30 +0900

    уник лог готов

--- 8_array/10_norm_task/10_log_uniq.c
+++ 8_array/10_norm_task/10_log_uniq.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -13,15 +14,38 @@ Log_entry scan_log_entry() {
   return entry;
 }
 
+bool is_inside(int *arr, int id, int size) {
+  bool result = false;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == id) {
+      result = true;
+      break;
+    }
+  }
+  return result;
+}
+
 int main() {
   int n;
   scanf("%d", &n);
   Log_entry log[n];
   memset(log, 0, sizeof(log));
+  int uniq_id_arr[n];
+  memset(uniq_id_arr, 0, sizeof(uniq_id_arr));
+  int count = 0;
 
   for (int i = 0; i < n; i++) {
     log[i] = scan_log_entry();
   }
 
+  for (int i = 0; i < n; i++) {
+    if (!is_inside(uniq_id_arr, log[i].id, n)) {
+      uniq_id_arr[i] = log[i].id;
+      count++;
+    }
+
+    uniq_id_arr[i] = log[i].id;
+  }
+  printf("%d\n", count);
   return 0;
 }
