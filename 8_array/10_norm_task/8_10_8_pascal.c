7841636ced726484565a61fb0375251e03c26596
Alexandr Ivanov <k4t4kun@gmail.com>
2024-02-11 12:38:31 +0900

    Треугольник Паскаля

--- 8_array/10_norm_task/8_10_8_pascal.c
+++ 8_array/10_norm_task/8_10_8_pascal.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    int arr[n][n];
+  int n;
+  scanf("%d", &n);
+  int arr[n + 1][n + 1];
+  memset(arr, 0, sizeof(arr));
+  arr[0][0] = 1;
 
-    arr[0][1] = 1;
-    arr[1][0] = 1;
-    arr[1][1] = 1;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            /* code */
-        }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= n; j++) {
+      if (j > 0) {
+        arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+      } else {
+        arr[i][j] = 1;
+      }
     }
+  }
 
-    return 0;
+  printf("\nOne line:\n");
+  for (int i = 0; i <= n; i++) {
+    printf("%d ", arr[n][i]);
+  }
+
+  printf("\n\nTriangle of Pascale:\n");
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= n; j++) {
+      printf("%d ", arr[i][j]);
+    }
+    printf("\n");
+  }
+
+  return 0;
 }
