4c200cfbe8b16301d60b6def6f6636bd29ef1ac0
Alexandr Ivanov <k4t4kun@gmail.com>
2024-02-11 16:24:48 +0900

    Решето Эратосфена готово

--- 8_array/10_norm_task/9_resheto_eratosphena.c
+++ 8_array/10_norm_task/9_resheto_eratosphena.c
@@ -1,15 +1,55 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_arr(char *arr, int size) {
+  for (int i = 0; i < size; i++) {
+    if (arr[i] != 45)
+      printf("%d ", arr[i]);
+    else
+      printf("%c ", arr[i]);
+  }
+  printf("\n");
+}
+
+void print_res(char *arr, int size) {
+  for (int i = 0; i < size; i++) {
+    if (arr[i] != '-') printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+void scan_arr(char *arr, int size) {
+  for (int i = 0; i < size; i++) {
+    scanf("%hhd", &arr[i]);
+  }
+}
+
 int main() {
   int N = 10;
   scanf("%d", &N);
-  int prime[N];
-  memset(prime, 0, sizeof(prime));
+  char prime[N];
+  scan_arr(prime, N);
 
+  // print_arr(prime, N);
+
+  // вычеркиваем 2
   for (int i = 0; i < N; i++) {
-    printf("prime[%d] = %d\n", i, prime[i]);
+    if (prime[i] % 2 == 0) prime[i] = '-';
   }
+  // print_arr(prime, N);
 
+  // вычеркиваем 3
+  for (int i = 0; i < N; i++) {
+    if (prime[i] % 3 == 0) prime[i] = '-';
+  }
+  // print_arr(prime, N);
+
+  // вычеркиваем 5
+  for (int i = 0; i < N; i++) {
+    if (prime[i] % 3 == 0) prime[i] = '-';
+  }
+  // print_arr(prime, N);
+
+  print_res(prime, N);
   return 0;
 }
