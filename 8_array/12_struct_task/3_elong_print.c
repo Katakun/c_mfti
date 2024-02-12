c894fa74a7df5c72fd02cce05dfefbfb940bcf22
Alexandr Ivanov <k4t4kun@gmail.com>
2024-02-12 12:50:49 +0900

    elong_print done

---
+++ 8_array/12_struct_task/3_elong_print.c
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+#define N 100
+
+typedef struct {
+  char a[N];       // number is a[0]*10^0 + a[1]*10^1 + ..+ a[n]*10^n
+  unsigned int n;  // наибольшая степень десяти
+} Decimal;
+
+void elong_print(Decimal n);
+
+int main() {
+  Decimal x = {{7, 4, 1}, 2};  // число 147
+  Decimal zero = {{0},
+                  0};  // число 0 представим как 0 умножить на 10 в степени 0
+
+  elong_print(x);     // 147
+  elong_print(zero);  // 0
+
+  return 0;
+}
+
+void elong_print(Decimal elong) {
+  for (int i = elong.n; i >= 0; i--) {
+    printf("%d", elong.a[i]);
+  }
+  printf("\n");
+}
+
+/*
+18 446 744 073 709 551 615
+12 345 678 901 234 567 890
+12 345 678 901 234 567 168
+*/
